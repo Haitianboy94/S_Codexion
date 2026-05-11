e89fe47df8c3f0627fa0cb37298256addc792b90
Ruthler Louis <ruthlerlouis@hotmail.com>
2026-05-11 22:48:15 +0200

    start

--- arg_parse.c
+++ arg_parse.c
@@ -12,33 +12,70 @@
 
 #include "codexion.h"
 
-int	parse_args(int argc, char **argv, t_args *out)
-{
-	if (argv[1] >= '0' && argv[1] <= '9')
-		out -> nb_coders = atoi(argv[1]);
-	if (argv[2] >= '0' && argv[2] <= '9')
-		out -> time_to_burnout = atoi(argv[2]);
-	if (argv[3] >= '0' && argv[3] <= '9')
-		out -> time_to_compile = atoi(argv[3]);
-	if (argv[4] >= '0' && argv[4] <= '9')
-		out -> time_to_debug = atoi(argv[4]);
-	if (argv[5] >= '0' && argv[5] <= '9')
-		out -> time_to_refactor = atoi(argv[5]);
-	if (argv[6] >= '0' && argv[6] <= '9')
-		out -> nb_compiles_required = atoi(argv[6]);
-	if (argv[7] >= '0' && argv[7] <= '9')
-		out -> dongle_cooldown = atoi(argv[7]);
-	if (argv[8] >= '0' && argv[8] <= '9')
-		out -> scheduler = atoi(argv[8]);
+static int check_arg(char *argv);
 
-	// Returns 0 on success, -1 on invalid input.
-    // Validates: all positive integers, scheduler == "fifo"|"edf"
+static int  fill_args(char **argv, t_args *out)
+{
+    if(!check_arg(argv[1]) || !check_arg(argv[2])
+		|| !check_arg(argv[3]) || !check_arg(argv[4])
+		|| !check_arg(argv[5]) || !check_arg(argv[6])
+        || !check_arg(argv[7])
+	)
+        return (write(2, "Invalid Argument\n", 17), -1);
+    out->nb_coders = atoi(argv[1]);
+    out->time_to_burnout = atol(argv[2]);
+    out->time_to_compile = atol(argv[3]);
+    out->time_to_debug = atol(argv[4]);
+    out->time_to_refactor = atol(argv[5]);
+    out->nb_compiles_required = atoi(argv[6]);
+    out->dongle_cooldown = atol(argv[7]);
+    return (0);
+}
+
+int parse_args(int argc, char **argv, t_args *out)
+{
+    if (argc != 9)
+        return (write(2, "\033[91mArgument Missing\033[0m", 25), -1);
+    if (fill_args(argv, out) == -1)
+        return (-1);
+    if (strcmp(argv[8], "FIFO") == 0)
+        out->scheduler = FIFO;
+    else if (strcmp(argv[8], "EDF") == 0)
+        out->scheduler = EDF;
+    else
+        return (write(2, "Choose between 'FIFO' or 'EDF'\n", 31), -1);
+    return (0);
 }
 	
-
-
-
-int	validate_args(const t_args *a)
+static int check_arg(char *argv)
 {
-	// Secondary check — returns 0 if consistent, -1 otherwise
+	int i;
+
+	i = 0;
+	while (argv[i] != '\0')
+	{
+		if (argv[i] < '0' || argv[i] > '9')
+			return 0;
+		i++;
+	}
+	return 1;
+}
+	
+int validate_args(const t_args *a)
+{
+    if (a->nb_coders < 1)
+        return (write(2, "Can't simulate 0 coders\n", 24), -1);
+    if (a->time_to_burnout <= 0)
+        return (write(2, "Time to burnout can't be 0\n", 27), -1);
+    if (a->time_to_compile <= 0)
+        return (write(2, "Time to compile can't be 0\n", 27), -1);
+    if (a->time_to_debug <= 0)
+        return (write(2, "Time to debug can't be 0\n", 25), -1);
+    if (a->time_to_refactor <= 0)
+        return (write(2, "Time to refactor can't be 0\n", 28), -1);
+    if (a->nb_compiles_required < 1)
+        return (write(2, "Need at least 1 compile\n", 24), -1);
+    if (a->dongle_cooldown < 0)
+        return (write(2, "dongle_cooldown can't be negative\n", 34), -1);
+    return (0);
 }
