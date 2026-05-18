370feddbfa0679462bf5c2b41b5009741a2614c3
Ruthler Louis <rulouis@f1r7s6.codam.nl>
2026-05-18 13:52:27 +0200

    start

--- heap.c
+++ heap.c
@@ -6,7 +6,7 @@
 /*   By: rulouis <rulouis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 09:36:05 by rulouis           #+#    #+#             */
-/*   Updated: 2026/05/11 12:42:33 by rulouis          ###   ########.fr       */
+/*   Updated: 2026/05/18 13:07:10 by rulouis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,24 @@
 int	heap_init(t_heap *h, int capacity)
 {
 	// Allocates h->data; returns 0 or -1
+	h = malloc(sizeof(t_heap));
+	if (!h)
+		return(-1);
+	return(0);
 }
 
 void    heap_free(t_heap *h)
 {
-	
+	free(h);
 }
 
 int	heap_push(t_heap *h, t_heapnode node)
 {
 	// Min-heap insert by node.priority; returns 0 or -1
+	if(node.priority)
+		return(0);
+	return(1);
+	
 }
 
 
